f26fad6de5d591241bdc2266f59a6c00a0f263c8
Fonebat <tim.jensen007@gmail.com>
2021-12-15 16:23:30 +0100

    subsanta

--- substitute-santa.py
+++ substitute-santa.py
@@ -3,16 +3,37 @@
 #TEINF-20
 #141221
 
+meny = input("""
+   Välj vad du vill göra
 
-options = list()
-genre = input("Namn på önskelistan: ")
-option = ""
-count = 1
-while option != "klar":
-   option = input("Option "+str(count)+": ")
-   if option != "done":
-      options.append(option)
-      count += 1
+   1. Skriv en önskelista
+   2. Läs en färdigskriven önskelista
+   3. Stäng ner
+""")
 
-with open(genre+'.txt', 'w+') as text_file:
-  text_file.write("\n".join(options))
+if "1" in meny:
+   önskor = list()
+   önskelista = input("Vems önskelista?: ")
+   print("Skriv 'Klar' när du vill avsluta din lista")
+   önska = ""
+   count = 1
+
+   while önska != "klar":
+      önska = input("Önska "+str(count)+": ")
+      if önska != "klar":
+         önskor.append(önska)
+         count += 1
+
+   with open(önskelista+'.txt', 'w+') as text_file:
+      text_file.write("\n".join(önskor))
+
+elif "2" in meny:
+   önskor = list()
+   önskelista = input("Vems önskelista?: ")
+   with open(önskelista+'.txt', 'r') as text_file:
+      lines = text_file.readlines()
+      for line in lines:
+         print(line, end="")
+
+elif "3" in meny:
+   exit()
